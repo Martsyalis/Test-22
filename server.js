ebbb83afc46505ad042edcea788eae8ab3565851
Maryus Martsyalis <martsyalis@gmail.com>
2021-08-09 22:54:58 -0600

    add express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("heelo World!");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+console.log("hello")
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
